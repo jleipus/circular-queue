866216ef2243a7f0f74180f1d04d5749d2095027
JLeipus <51248052+jleipus@users.noreply.github.com>
2022-04-27 15:19:20 +0300

    Add files via upload

---
+++ src/CircularQueue.java
@@ -0,0 +1,65 @@
+public class CircularQueue {
+
+    private static boolean LOGGING_MODE = false;
+
+    private int front, rear; // Buffer front and rear markers
+    private int[] nums; // buffer array
+
+    public CircularQueue(int initialSize) {
+
+        this.front = this.rear = -1; // -1 means the buffer is empty
+        this.nums = new int[initialSize]; // Creating array of specified length
+    }
+
+    public void enqueue(int data) throws InterruptedException {
+
+        synchronized (this) {
+            if (isFull()) { // If buffer is full, waiting for space
+                if(LOGGING_MODE) System.out.println("Enqueue: waiting for slot.");
+                wait();
+                if(LOGGING_MODE) System.out.println("Enqueue: slot received.");
+            } else if (isEmpty()) {
+                front++;
+            }
+
+            rear = rear + 1;
+            nums[rear % nums.length] = data;
+
+            notify(); // Notifying that data has been added
+            if(LOGGING_MODE) System.out.println("Enqueued " + data);
+        }
+    }
+
+    public int dequeue() throws InterruptedException {
+
+        synchronized (this) {
+            if (isEmpty()) { // If buffer is empty, witing for data
+                if(LOGGING_MODE) System.out.println("Dequeue: waiting for data.");
+                wait();
+                if(LOGGING_MODE) System.out.println("Dequeue: data received.");
+            }
+
+            int temp = nums[front % nums.length];
+            if (front == rear) {
+                front = rear = -1;
+            } else {
+                front = front + 1;
+            }
+
+            notify(); // Notifying that space has been freed
+            if(LOGGING_MODE) System.out.println("Dequeued " + temp);
+
+            return temp;
+        }
+    }
+
+    private boolean isEmpty() {
+
+        return front == -1;
+    }
+
+    private boolean isFull() {
+
+        return (rear + 1) % nums.length == front % nums.length;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    // Program reads numbers from a file until it finds -1
+    // Starts two threads, one for reading and one for writing
+    // Reading thread pushes to the queue, writing reads pulls from it
+
+    public static int BUFFER_SIZE = 3; // Buffer length parameter
+    public static int READER_DELAY_MS = 0; // Reading thread delay
+    public static int WRITER_DELAY_MS = 0; // Writing thread delay
+
+    public static void main(String[] args) {
+
+        // Creating queue of specified length
+        CircularQueue queue = new CircularQueue(BUFFER_SIZE);
+
+        try {
+            // Starting the reading thread
+            Reader reader = new Reader(queue, args[0]);
+            reader.start();
+
+            // Startoing the writing thread
+            Writer writer = new Writer(queue);
+            writer.start();
+
+            // Waiting for both threads to finish
+            reader.join();
+            writer.join();
+        } catch (InterruptedException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
+
+class Reader extends Thread {
+
+    private CircularQueue queue;
+    private String inputFile;
+
+    public Reader(CircularQueue _syncObj, String _inpuString) {
+
+        this.queue = _syncObj;
+        this.inputFile = _inpuString;
+    }
+
+    public void run() {
+
+        // String inputFile = "in.data";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                if(Main.READER_DELAY_MS > 0) Thread.sleep(Main.READER_DELAY_MS); // Pausing the thread
+                queue.enqueue(Integer.parseInt(line)); // Pushing the value from the file to the queue
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Writer extends Thread {
+
+    private CircularQueue queue;
+
+    public Writer(CircularQueue _syncObj) {
+
+        this.queue = _syncObj;
+    }
+
+    public void run() {
+
+        String outputFile = "out.data";
+
+        try (FileWriter writer = new FileWriter(outputFile)) {
+            while (true) {
+                if(Main.WRITER_DELAY_MS > 0) Thread.sleep(Main.WRITER_DELAY_MS); // Pausing the thread
+                int data = queue.dequeue(); // Pulling value from the buffer
+
+                writer.write(Integer.toString(data));
+                if(data == -1) { // If value is -1, stopping reader
+                    break;
+                } else {
+                    writer.write("\n");
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
